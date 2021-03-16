58910f39fbe74670b0276f6d7094f2ed6145cc20
Alexey Zimarev <alex@zimarev.com>
2021-03-16 10:39:25 +0100

    Added DefaultStack

--- AutoDevOps/DefaultStack.cs
+++ AutoDevOps/DefaultStack.cs
@@ -1,11 +1,26 @@
+using System;
 using Pulumi;
 
 namespace AutoDevOps {
-    public class AppStack : Stack {
-        public AppStack() {
-            var config   = new Config();
-            var settings = new AutoDevOpsSettings(config);
-            var autoDevOps = new AutoDevOps(settings);
+    public class DefaultStack : Stack {
+        protected readonly AutoDevOps AutoDevOps;
+
+        public DefaultStack() : this(settings => new AutoDevOps(settings)) { }
+
+        public DefaultStack(Func<AutoDevOpsSettings, AutoDevOps> cfg) {
+            var config     = new Config();
+            var settings   = new AutoDevOpsSettings(config);
+            AutoDevOps = cfg(settings);
         }
+    }
+
+    public class DefaultStack<T> : DefaultStack {
+        public DefaultStack(Func<AutoDevOps, Output<T>> getOutput) => AppStackOutput = getOutput(AutoDevOps);
+
+        public DefaultStack(Func<AutoDevOpsSettings, AutoDevOps> cfg, Func<AutoDevOps, Output<T>> getOutput) : base(cfg)
+            => AppStackOutput = getOutput(AutoDevOps);
+
+        [Output]
+        public Output<T> AppStackOutput { get; }
     }
 }
